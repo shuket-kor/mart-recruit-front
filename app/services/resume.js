b5f1bb741ab8b02254fc0dd628ec0e55211e951d
nc.seong <atomix@hanmail.net>
2021-05-14 15:14:44 +0900

    구인공고 작업

---
+++ app/services/resume.js
@@ -0,0 +1,7 @@
+const logger = require('../config/logger.js');
+const secretKey = require('../config/secretKey').secretKey;
+const got = require("got");
+
+module.exports = class resumeService {
+    
+}
